b2032e5472eb759a6b2791e5c42ce2d93aa39b3f
BurhanuddinLokhandwala10 <burlokh23@gmail.com>
2026-06-03 10:38:39 +0530

    sub

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,14 @@
 import pytest
 from calculator import add
+from calculator import sub
 
 def test_add():
     assert add(4,5) == 9
     assert add(1,2) == 3
+
+
+def test_sub():
+    assert sub(5,4) == 1
+    assert sub(2,1) == 1
+    assert sub(10,1) == 9
     
